854b6e6fd7104a30a1bc6a53c49a8550f3ffb156
Muhammad Hermas Yuda Pamungkas <hermas.yuda@gmail.com>
2022-06-25 09:36:56 +0700

    set database url into env

--- src/main/kotlin/com/catnip/groceriesapp/component/db/DatabaseComponent.kt
+++ src/main/kotlin/com/catnip/groceriesapp/component/db/DatabaseComponent.kt
@@ -10,15 +10,9 @@ import org.springframework.stereotype.Component
 
 @Component
 class DatabaseComponent {
-/*
-    @Value("\${DATABASE_URL}")
-    private lateinit var databaseUrl : String*/
 
-    companion object{
-        private const val DATABASE_URL = "mongodb+srv://hermasyuda:t6VWrtRBWihj8JNc@cluster0.pwcyn.mongodb.net/?retryWrites=true&w=majority"
-    }
-
-    private val database: MongoClient = KMongo.createClient(DATABASE_URL)
+    private val databaseUrl = System.getenv("DATABASE_URL")
+    private val database: MongoClient = KMongo.createClient(databaseUrl)
 
     fun groceriesCollection(): MongoCollection<Grocery> = database.getDatabase("groceries").getCollection()
 
